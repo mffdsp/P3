e8ad2bd085a4e8515b02126197a2dd4f5114fade
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2019-06-05 12:13:40 -0300

    Update Main.java

--- Projeto01/FolhaDePagamento/src/Main.java
+++ Projeto01/FolhaDePagamento/src/Main.java
@@ -115,7 +115,7 @@ public class Main {
 
     private static void clearScreen(int time)throws InterruptedException {
         dormindinho(0);
-        System.out.print("\033[H\033[2J");
+        //System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
